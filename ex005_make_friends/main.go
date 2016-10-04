8b74fa0e54a65a6dbbe917419531eebb3e929ca9
Vanessa Virgitti <virgitti@gmail.com>
2016-10-04 15:16:17 +0100

    Passed the test for list of friends

--- ex005_make_friends/main.go
+++ ex005_make_friends/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 
+
 }
 
 type person struct {
@@ -12,13 +13,30 @@ type person struct {
 	friends []string
 }
 
-func (p person) makeFriend(p2 person) person {
-	fmt.Println("PERSON WITHIN THE FUNCTION", p)
-	fmt.Println("PERSON WITHIN THE FUNCTION 2", p2)
+//func (p person) makeFriend(p2 person) person {
+//	fmt.Println("PERSON WITHIN THE FUNCTION", p)
+//	fmt.Println("PERSON WITHIN THE FUNCTION 2", p2)
+//
+//	p.friends = append(p.friends, p2.name)
+//	//p2.friends = append(p2.friends, p.name)
+//	fmt.Println("PERSON END OF FUNCTION", p)
+//	//fmt.Println("PERSON END OF FUNCTION", p2)
+//	return p
+//}
+
+func makeFriends(p person, p2 person) (person, person) {
 
 	p.friends = append(p.friends, p2.name)
-	//p2.friends = append(p2.friends, p.name)
+	p2.friends = append(p2.friends, p.name)
+
 	fmt.Println("PERSON END OF FUNCTION", p)
-	//fmt.Println("PERSON END OF FUNCTION", p2)
-	return p
+	fmt.Println("PERSON END OF FUNCTION", p2)
+
+	return p, p2
+}
+
+func listFriends(p person) []string {
+
+	return p.friends
+
 }
