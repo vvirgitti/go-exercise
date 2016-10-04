648ad0c94da5d4647baf974c4f908252bd78b810
Vanessa Virgitti <virgitti@gmail.com>
2016-10-04 14:36:55 +0100

    Progresses... test failing for the method makeFriend

--- ex005_make_friends/main.go
+++ ex005_make_friends/main.go
@@ -1,11 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
-	// your code here
+
 }
 
 type person struct {
 	name string
 	age int
 	friends []string
+}
+
+func (p person) makeFriend(p2 person) person {
+	fmt.Println("PERSON WITHIN THE FUNCTION", p)
+	fmt.Println("PERSON WITHIN THE FUNCTION 2", p2)
+
+	p.friends = append(p.friends, p2.name)
+	//p2.friends = append(p2.friends, p.name)
+	fmt.Println("PERSON END OF FUNCTION", p)
+	//fmt.Println("PERSON END OF FUNCTION", p2)
+	return p
 }
