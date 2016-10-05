9d68940956952aefa9cd229c5ad001964704dd7b
Vanessa Virgitti <virgitti@gmail.com>
2016-10-05 09:41:31 +0100

    Quick clean up

--- ex005_make_friends/main.go
+++ ex005_make_friends/main.go
@@ -19,9 +19,6 @@ func makeFriends(p *person, p2 *person) {
 	p.friends = append(p.friends, p2.name)
 	p2.friends = append(p2.friends, p.name)
 
-	fmt.Println("PERSON END OF FUNCTION", p)
-	fmt.Println("PERSON END OF FUNCTION", p2)
-
 }
 
 func listFriends(p person) []string {
